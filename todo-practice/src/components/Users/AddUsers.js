afe037da35784a37a7c2861cac9c35b65ad999d0
meyash <yashsn2127@gmail.com>
2021-06-21 11:56:12 +0530

    useRef

--- todo-practice/src/components/Users/AddUsers.js
+++ todo-practice/src/components/Users/AddUsers.js
@@ -2,31 +2,41 @@ import React from "react";
 
 import Card from "../UI/Card";
 import Button from "../UI/Button";
-import { useState } from "react";
+import { useRef } from "react";
+// import { useState } from "react";
 
 const AddUser = (props) => {
-  const [name, setName] = useState("");
-  const [age, setAge] = useState("");
-  const nameHandler = (e) => {
-    e.preventDefault();
-    setName(e.target.value);
-  };
-  const ageHandler = (e) => {
-    e.preventDefault();
-    setAge(e.target.value);
-  };
+  const refName = useRef();
+  const refAge = useRef();
+
+  // const [name, setName] = useState("");
+  // const [age, setAge] = useState("");
+
+  // const nameHandler = (e) => {
+  //   e.preventDefault();
+  //   setName(e.target.value);
+  // };
+  // const ageHandler = (e) => {
+  //   e.preventDefault();
+  //   setAge(e.target.value);
+  // };
+
   const submitHandler = (e) => {
     e.preventDefault();
+    const name = refName.current.value;
+    const age = refAge.current.value;
     props.addHandler(name, age);
   };
   return (
     <Card>
       <form onSubmit={submitHandler}>
         <label htmlFor="uname">Name: </label>
-        <input onChange={nameHandler} type="text" id="uname"></input>
+        {/* <input onChange={nameHandler} type="text" id="uname"></input> */}
+        <input ref={refName} type="text" id="uname"></input>
         <br />
         <label htmlFor="uage">Age: </label>
-        <input onChange={ageHandler} type="text" id="uage"></input>
+        {/* <input onChange={ageHandler} type="text" id="uage"></input> */}
+        <input ref={refAge} type="text" id="uage"></input>
         <br />
         <Button name="Submit" type="submit" />
       </form>
